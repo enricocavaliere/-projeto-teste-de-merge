228697a755130584604af4be3b5f776e7aee0bb4
Enrico <enricocavaliere56@gmail.com>
2025-11-07 11:55:01 -0300

    if sim e não

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Ola mundo");
 
 let churrasco = "sim";
 
-if( churrasco == "sim") {
+if( churrasco = "!não") {
     alert("vai ter churrasco");
 } else{
     alert("Então vamos comer churrasco!");
